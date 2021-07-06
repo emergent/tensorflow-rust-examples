fa63288db6cd65b036d7761af6ae1a9972ed108e
Satoshi Yoshikawa <yoshikawa@fairydevices.jp>
2021-07-07 00:33:53 +0900

    add fashion mnist

---
+++ fashion_mnist/generate.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+from PIL import Image
+import os
+
+print('tensorflow version is {}'.format(tf.__version__))
+
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images,
+                               test_labels) = fashion_mnist.load_data()
+
+class_names = ['Top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Boot']
+
+
+def save_img(filename, data):
+    img = Image.new('L', (28, 28))
+    pix = img.load()
+    for i in range(28):
+        for j in range(28):
+            pix[i, j] = int(data[j][i])
+    img2 = img.resize((28, 28))
+    img2.save(filename)
+
+
+def dump(data, dhead):
+    for i in range(10):
+        dname = "{}/{}".format(dhead, class_names[i])
+        if os.path.isdir(dname) is False:
+            os.makedirs(dname)
+    images, labels = data
+    count = [0]*10
+    for i in range(len(images)):
+        index = labels[i]
+        filename = '{}/{}/{}.png'.format(dhead,
+                                         class_names[index], count[index])
+        save_img(filename, images[i])
+        count[index] += 1
+        print('{}     \r'.format(filename), end='')
+    print('done                   ')
+
+
+# thanks from https://github.com/kaityo256/fashion_mnist_dump
+print('saving test images')
+dump((test_images, test_labels), "images")
+
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+# build the model
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+# compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# train the model
+model.fit(train_images, train_labels, epochs=5)
+model.save('models')
---
+++ fashion_mnist/src/main.rs
@@ -0,0 +1,48 @@
+use anyhow::{anyhow, ensure, Result};
+use image::io::Reader as ImageReader;
+use std::path::PathBuf;
+use tensorflow::{Graph, SavedModelBundle, SessionOptions, SessionRunArgs, Tensor};
+
+const MODEL_DIR: &str = "fashion_mnist/models";
+const IMAGE_FILE: &str = "fashion_mnist/images/Dress/0.png";
+
+fn main() -> Result<()> {
+    let path = PathBuf::from(MODEL_DIR);
+    ensure!(path.exists(), anyhow!("directory not found"));
+
+    // loading image
+    let img = ImageReader::open(IMAGE_FILE)?.decode()?;
+
+    let values = img
+        .to_luma8()
+        .to_vec()
+        .into_iter()
+        .map(|b| b as f32 / 255.0)
+        .collect::<Vec<f32>>();
+    let img_tensor = Tensor::<f32>::new(&[28, 28]).with_values(&values)?;
+
+    let mut graph = Graph::new();
+    let bundle = SavedModelBundle::load(&SessionOptions::new(), &["serve"], &mut graph, &path)?;
+    let session = &bundle.session;
+
+    let meta = bundle.meta_graph_def();
+    let signature = meta.get_signature("serving_default")?;
+
+    let input_info = signature.get_input("flatten_input")?;
+    let input_op = graph.operation_by_name_required(&input_info.name().name)?;
+
+    let output_info = signature.get_output("dense_1")?;
+    let output_op = graph.operation_by_name_required(&output_info.name().name)?;
+
+    let mut run_args = SessionRunArgs::new();
+    run_args.add_feed(&input_op, input_info.name().index, &img_tensor);
+
+    let token = run_args.request_fetch(&output_op, output_info.name().index);
+    session.run(&mut run_args)?;
+
+    let output = run_args.fetch::<f32>(token)?;
+    let res = output.to_vec();
+    println!("{:?}", res);
+
+    Ok(())
+}
